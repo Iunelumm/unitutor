630f9532efe944e258d540015760f31a5fcf87d5
Iunelumm <87514081+Iunelumm@users.noreply.github.com>
2025-11-17 22:02:25 -0800

    Add files via upload

---
+++ client/src/App.tsx
@@ -0,0 +1,63 @@
+import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/NotFound";
+import { Route, Switch } from "wouter";
+import ErrorBoundary from "./components/ErrorBoundary";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import Home from "./pages/Home";
+import StudentDashboard from "./pages/StudentDashboard";
+import TutorDashboard from "./pages/TutorDashboard";
+import StudentProfile from "./pages/StudentProfile";
+import TutorProfile from "./pages/TutorProfile";
+import FindTutors from "./pages/FindTutors";
+import Sessions from "./pages/Sessions";
+import SessionDetail from "./pages/SessionDetail";
+import Support from "./pages/Support";
+import AdminDashboard from "./pages/AdminDashboard";
+import RoleSelection from "./pages/RoleSelection";
+import Terms from "./pages/Terms";
+import AdminLogin from "./pages/AdminLogin";
+import MyTutors from "./pages/MyTutors";
+import FilesAndNotes from "./pages/FilesAndNotes";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path={"/"} component={Home} />
+      <Route path={"/role-selection"} component={RoleSelection} />
+      <Route path={"/terms"} component={Terms} />
+      <Route path={"/admin-login"} component={AdminLogin} />
+      <Route path={"/student"} component={StudentDashboard} />
+      <Route path={"/student/profile"} component={StudentProfile} />
+      <Route path={"/student/find-tutors"} component={FindTutors} />
+      <Route path={"/student/sessions"} component={Sessions} />
+      <Route path={"/student/sessions/:id"} component={SessionDetail} />
+      <Route path={"/student/my-tutors"} component={MyTutors} />
+      <Route path={"/student/files"} component={FilesAndNotes} />
+      <Route path={"/tutor"} component={TutorDashboard} />
+      <Route path={"/tutor/profile"} component={TutorProfile} />
+      <Route path={"/tutor/sessions"} component={Sessions} />
+      <Route path={"/tutor/sessions/:id"} component={SessionDetail} />
+      <Route path={"/support"} component={Support} />
+      <Route path={"/admin"} component={AdminDashboard} />
+      <Route path={"/404"} component={NotFound} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <ErrorBoundary>
+      <ThemeProvider defaultTheme="light">
+        <TooltipProvider>
+          <Toaster />
+          <Router />
+        </TooltipProvider>
+      </ThemeProvider>
+    </ErrorBoundary>
+  );
+}
+
+export default App;
+
